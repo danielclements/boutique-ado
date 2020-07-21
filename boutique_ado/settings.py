47cf1acfe537ad53d92c697a1924ae1e10ea6553
Daniel Clements <daniel.clements14@gmail.com>
2020-07-21 04:18:44 +0000

    added products page

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -150,5 +150,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URLS = '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-] + static(settings.MEDIA_URLS, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
